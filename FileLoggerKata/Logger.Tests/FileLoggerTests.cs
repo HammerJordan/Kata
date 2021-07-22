6d5326b2e0b9046c375066fba281146bac56dab9
Jordan <hammerleejordan@gmail.com>
2021-07-22 12:33:18 -0700

    Step 3
    Messages should default to having a prefix of "YYYY-MM-DD HH:MM:SS " in front of the message. For example, Log("test") would yield "2020-07-14 10:25:23 test". Time can be in local timezone and should use 24-hour clock.

--- FileLoggerKata/Logger.Tests/FileLoggerTests.cs
+++ FileLoggerKata/Logger.Tests/FileLoggerTests.cs
@@ -1,7 +1,49 @@
-﻿namespace FileLoggerKata.Logger.Tests
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using Xunit;
+using FluentAssertions;
+
+namespace FileLoggerKata.Logger.Tests
 {
     public class FileLoggerTests
     {
-        
+        private readonly FileLogger logger;
+
+        public FileLoggerTests()
+        {
+            logger = new FileLogger();
+        }
+
+        public string ReadLastLineInLogFile()
+        {
+            var line = File.ReadLines(logger.PathToCurrentLogFile).Last();
+            if (line.Contains(Environment.NewLine))
+                line = line.Replace(Environment.NewLine,"");
+            return line;
+
+        }
+
+        [Fact]
+        public void CanCallLog()
+        {
+            logger.Log("My Msg");
+        }
+
+        [Fact]
+        public void LogLogsFileWithCurrentTimeStamp()
+        {
+            var time = DateTime.UtcNow.ToString(CultureInfo.InvariantCulture);
+            string timeStamp = DateTime.Now.ToString("yyyy-mm-dd hh:mm:ss");
+
+            logger.Log(time);
+
+            string lastLog = ReadLastLineInLogFile();
+            string expected = $"{timeStamp} {time}";
+
+            lastLog.Should().BeEquivalentTo(expected);
+        }
     }
 }
--- FileLoggerKata/Logger/FileLogger.cs
+++ FileLoggerKata/Logger/FileLogger.cs
@@ -1,10 +1,27 @@
-﻿namespace FileLoggerKata.Logger
+﻿using System;
+using System.IO;
+using System.Net.Mime;
+
+namespace FileLoggerKata.Logger
 {
     public class FileLogger
     {
+        private readonly string pathToLogFile = @"C:\Users\Hammer\Desktop\MyProjects\C#\Kata\FileLoggerKata\Logs";
+        private readonly string logFile = "log.txt";
+
+        public string PathToCurrentLogFile => Path.Join(pathToLogFile, logFile);
+        
         public void Log(string message)
         {
+            string loggedMessage = $"{GetTimeStamp()} {message}\n";
             
+            File.AppendAllText(PathToCurrentLogFile,loggedMessage);
+        }
+
+        private string GetTimeStamp()
+        {
+            string timeStamp = DateTime.Now.ToString("yyyy-mm-dd hh:mm:ss");
+            return timeStamp;
         }
     }
 }
