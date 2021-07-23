c1c6d71153935b47ced476be04178d6f27ff131e
Jordan <hammerleejordan@gmail.com>
2021-07-23 13:36:09 -0700

    Step 5
    Now update the method so that it writes to a file called logYYYYMMDD.txt, where YYYYMMDD corresponds to the current date. Note: It's not unusual for the logger instance to run for a long period of time, and the log messages should go into the appropriate file based on when they are logged, not when the logger was created.

--- FileLoggerKata/Logger.Tests/FileLoggerTests.cs
+++ FileLoggerKata/Logger.Tests/FileLoggerTests.cs
@@ -21,9 +21,8 @@ namespace FileLoggerKata.Logger.Tests
         {
             var line = File.ReadLines(logger.PathToCurrentLogFile).Last();
             if (line.Contains(Environment.NewLine))
-                line = line.Replace(Environment.NewLine,"");
+                line = line.Replace(Environment.NewLine, "");
             return line;
-
         }
 
         [Fact]
@@ -45,5 +44,45 @@ namespace FileLoggerKata.Logger.Tests
 
             lastLog.Should().BeEquivalentTo(expected);
         }
+
+        [Fact]
+        public void LogCreatesNewFileWhenFileDoesNotExist()
+        {
+            var time = DateTime.UtcNow.ToString(CultureInfo.InvariantCulture);
+            string timeStamp = DateTime.Now.ToString("yyyy-mm-dd hh:mm:ss");
+
+            if (File.Exists(logger.PathToCurrentLogFile))
+                File.Delete(logger.PathToCurrentLogFile);
+
+            logger.Log(time);
+
+            string lastLog = ReadLastLineInLogFile();
+            string expected = $"{timeStamp} {time}";
+
+            lastLog.Should().BeEquivalentTo(expected);
+        }
+        
+        [Fact]
+        public void LogShouldLogInAFileWithTheCurrentDate()
+        {
+            var time = DateTime.UtcNow.ToString(CultureInfo.InvariantCulture);
+            string timeStamp = DateTime.Now.ToString("yyyy-mm-dd hh:mm:ss");
+            string dateStamp = DateTime.Now.ToString("yyyyMMdd");
+            
+            logger.Log(time);
+
+            string path = Path.Combine(logger.LogDirectory, $"{dateStamp}.txt");
+
+            File
+                .Exists(Path.Combine(logger.LogDirectory, $"{dateStamp}.txt"))
+                .Should()
+                .BeTrue();
+
+            string lastLog = ReadLastLineInLogFile();
+            string expected = $"{timeStamp} {time}";
+
+            lastLog.Should().BeEquivalentTo(expected);
+        }
+        
     }
 }
--- FileLoggerKata/Logger/FileLogger.cs
+++ FileLoggerKata/Logger/FileLogger.cs
@@ -7,21 +7,27 @@ namespace FileLoggerKata.Logger
     public class FileLogger
     {
         private readonly string pathToLogFile = @"C:\Users\Hammer\Desktop\MyProjects\C#\Kata\FileLoggerKata\Logs";
-        private readonly string logFile = "log.txt";
 
-        public string PathToCurrentLogFile => Path.Join(pathToLogFile, logFile);
+        public string PathToCurrentLogFile => Path.Join(pathToLogFile, $"{GetDateStamp()}.txt");
+        public string LogDirectory => pathToLogFile;
+        
         
         public void Log(string message)
         {
-            string loggedMessage = $"{GetTimeStamp()} {message}\n";
+            string loggedMessage = $"{GetDateTimeStamp()} {message}\n";
             
             File.AppendAllText(PathToCurrentLogFile,loggedMessage);
         }
 
-        private string GetTimeStamp()
+        private string GetDateTimeStamp()
         {
             string timeStamp = DateTime.Now.ToString("yyyy-mm-dd hh:mm:ss");
             return timeStamp;
+        }
+
+        private string GetDateStamp()
+        {
+            return DateTime.Now.ToString("yyyyMMdd");
         }
     }
 }
